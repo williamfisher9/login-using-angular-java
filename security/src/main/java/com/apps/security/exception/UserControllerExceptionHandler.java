c503f47fee05e54e658fa4fec5a30c5043f9c9d3
williamfisher9 <fisher.a.william@gmail.com>
2024-09-18 05:57:17 -0500

    handled method argument not valid exception in the exception handler advice

--- security/src/main/java/com/apps/security/exception/UserControllerExceptionHandler.java
+++ security/src/main/java/com/apps/security/exception/UserControllerExceptionHandler.java
@@ -3,8 +3,14 @@ package com.apps.security.exception;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class UserControllerExceptionHandler {
@@ -12,5 +18,17 @@ public class UserControllerExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException exc){
         return new ResponseEntity<>("ERROR", HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exc) {
+        Map<String, Object> errorsMap = new HashMap<>();
+
+        List<String> errorsList = exc.getBindingResult().getFieldErrors().stream()
+                .map(field -> field.getDefaultMessage()).toList();
+
+        errorsMap.put("errors", errorsList);
+
+        return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST);
     }
 }
